c1094f81f8e61320b465a5898492a7639beed6f5
Daniel Fodor <fodordaniel89@gmail.com>
2021-03-16 20:51:07 +0100

    basic markup and setup

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,21 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./src/css'))
 });
 
+// gulp.task('production', function () {
+//     return gulp.src('./src/scss/style.scss')
+//       // Compile SASS files
+//       .pipe(sass({
+//         outputStyle: 'nested',
+//         onError: console.error.bind(console, 'Sass error:')
+//       }))
+//       // Auto-prefix css styles for cross browser compatibility
+//       .pipe(autoprefixer())
+//       // Minify the file
+//       .pipe(csso({ sourceMap: true }))
+//       // Output
+//       .pipe(gulp.dest('./src/css'))
+// });
+
 gulp.task('watch', function(cb) {
   gulp.watch('src/scss/**/**.scss', gulp.series('styles'));
 });
